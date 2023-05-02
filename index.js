f422976795cc6593d1c6ed29b51af2da610a3945
divyanshigkmit <divyanshi@gkmit.co>
2023-05-02 17:47:05 +0530

    feat: database connectivity

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 
 const app = require('./app');
+const { sequelize } = require('./models');
 
 const startServer = async function () {
   try {
-    // await sequelize.authenticate();
-    // console.log("... Microservice db ✔");
+    await sequelize.authenticate();
+    console.log('... Microservice db ✔');
     app.listen(process.env.SERVER_PORT);
     console.log(`--- Server started on ${process.env.SERVER_PORT} ---\n\n`);
   } catch (err) {
